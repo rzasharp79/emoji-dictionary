c3347576e6ab9dfebe08fa0cf02e03e74636b2d7
James Todd <james.c.todd@gmail.com>
2017-10-19 04:49:53 -0400

    added segue and navigation controller

--- KarmaChameleon/ViewController.swift
+++ KarmaChameleon/ViewController.swift
@@ -31,6 +31,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "🕌")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
